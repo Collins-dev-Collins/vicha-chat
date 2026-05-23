6807e7461725e54d96c24f09a0d851d48ac5ddbd
Collins-dev-Collins <mfanelo01tex@gmail.com>
2026-05-23 12:28:16 +0200

    Improved layout

--- app.py
+++ app.py
@@ -17,15 +17,19 @@ def chat():
         user_input = request.form.get("message")
 
         if user_input:
+            chat_history.append({"role": "user", "content":user_input})
 
             response = client.models.generate_content(
                 model="gemini-3.5-flash",
-                contents=user_input
+                contents=[msg["content"] for msg in chat_history]
             )
-            chat_history.append(("You: ",user_input))
-            chat_history.append(("AI: ",response.text))
+            #chat_history.append(("You: ",user_input))
+            #chat_history.append(("AI: ",response.text))
+            #chat_history.append({"role":"ai","content":ai_reply})
+            ai_reply = response.text
+            chat_history.append({"role":"ai","content":ai_reply})
 
-    return render_template("index.html",chat=chat_history)
+    return render_template("index.html",chat_history=chat_history)
 
 if __name__ == "__main__":
     app.run(debug=True)
